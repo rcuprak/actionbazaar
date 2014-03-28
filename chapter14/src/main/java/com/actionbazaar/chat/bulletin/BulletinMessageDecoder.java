4c7a284b94bcdc77aa3faf6a7616cad662ac553e
Ryan Cuprak <rcuprak@gmail.com>
2014-03-28 15:25:48 -0400

    Merging from laptop.

---
+++ chapter14/src/main/java/com/actionbazaar/chat/bulletin/BulletinMessageDecoder.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.actionbazaar.chat.bulletin;
+
+import javax.websocket.DecodeException;
+import javax.websocket.Decoder;
+import javax.websocket.EncodeException;
+import javax.websocket.Encoder;
+import javax.websocket.EndpointConfig;
+
+/**
+ *
+ * @author rcuprak
+ */
+public class BulletinMessageDecoder implements Decoder.Text<BulletinMessage> {
+
+    @Override
+    public BulletinMessage decode(String arg0) throws DecodeException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean willDecode(String arg0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void init(EndpointConfig config) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void destroy() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+   
+    
+}
