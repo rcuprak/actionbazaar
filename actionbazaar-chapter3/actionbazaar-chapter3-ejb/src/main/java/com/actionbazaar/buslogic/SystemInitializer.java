0361323e7565922a3096a6fb1657996ad5dc5637
Kiran Mohan <kiranmohan@123.com>
2015-12-08 10:36:48 +0530

    create tables in chapter3 during deployment

--- actionbazaar-chapter3/actionbazaar-chapter3-ejb/src/main/java/com/actionbazaar/buslogic/SystemInitializer.java
+++ actionbazaar-chapter3/actionbazaar-chapter3-ejb/src/main/java/com/actionbazaar/buslogic/SystemInitializer.java
@@ -16,9 +16,19 @@
  */
 package com.actionbazaar.buslogic;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
 import javax.ejb.Asynchronous;
+import javax.ejb.PostActivate;
+import javax.ejb.PrePassivate;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import javax.sql.DataSource;
 
 /**
  * Initializes application logging.
@@ -27,6 +37,79 @@ import javax.ejb.Startup;
 @Startup
 @Asynchronous
 public class SystemInitializer {
+	
+    /**
+     * Creates a data source
+     * The datasource is defined in glassfish-resources.xml in the EAR.
+     * This is a DERBY in-memory database. 
+     */
+    @Resource(name = "java:app/jdbc/ActionBazaarDS")
+    private DataSource dataSource;
+    
+    /**
+     * Connection info
+     */
+    private Connection connection;
+    
+    @PostConstruct
+    public void initDatabaseSchema() {
+    	try {
+			connection = dataSource.getConnection();
+			Statement statement = connection.createStatement();
+			String createBiddersTable = "create table BIDDERS ("
+											+ "username varchar(10) PRIMARY KEY,"
+											+ "first_name varchar(30) NOT NULL,"
+											+ "credit_card_type varchar(20) NOT NULL"
+											+ ")";
+			
+			String createBidsTable = "create table BIDS (" 
+											+ "BID_ID numeric(19) PRIMARY KEY,"
+											+ "BID_DATE DATE NOT NULL,"
+											+ "BID_STATUS varchar(20) NOT NULL,"
+											+ "BID_PRICE numeric(19,4) NOT NULL,"
+											+ "BID_ITEM_ID numeric(19) NOT NULL,"
+											+ "BID_BIDDER varchar(45) NOT NULL"
+											+ ")";
+
+			statement.execute(createBiddersTable);
+			statement.execute(createBidsTable);
+			statement.close();
+			connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
+    @PostActivate
+    public void openConnection() {
+    	System.out.println("*** System Initializer PostActivate. ***");
+    }
+    
+    @PrePassivate
+    public void closeConnection() {
+    	System.out.println("*** System Initializer Pre-Passivate. ***");
+    }
+    
+    @PreDestroy
+    public void cleanup() {
+        try {
+        	connection = dataSource.getConnection();
+        	
+        	// Since this is just a sample application, 
+        	// drop the tables when the application is removed.
+        	// This is not usually done in production applications.
+        	Statement statement = connection.createStatement();
+        	statement.execute("drop table BIDDERS");
+        	statement.execute("drop table BIDS");
+        	statement.close();
+        	
+            connection.close();
+            connection = null;
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+    }
 
     @Asynchronous
     public void updateDatabaseSchema() {
